3c07368c32a5c7eaed19f6d620b466b38632cfae
BernabeiPietro <pietrobernaei@gmail.com>
2021-05-01 13:31:44 +0200

    Add failing Test

---
+++ CIExample/src/test/java/com/ciexample/CIExample/App2Test.java
@@ -0,0 +1,20 @@
+package com.ciexample.CIExample;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class App2Test {
+	private App app;
+	@Before
+	private void setup() {
+		// TODO Auto-generated method stub
+		app=new App();
+	}
+	@Test
+	public void testSayHello() {
+	assertEquals("hallo", app.sayHello());
+	}
+
+}
